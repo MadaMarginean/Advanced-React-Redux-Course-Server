825ac830bd2fb494a16e176165df1051bed57ea0
Madalina Marginean <madalina.marginean@mcro.tech>
2019-01-09 15:38:54 +0200

    Server Setup, App Setup, some configuration

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan'); // morgan is a login framework
+const app = express();
+
+// App Setup
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+
+// Server Setup
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+
+server.listen(port);
+console.log('Server listening on:', port);
