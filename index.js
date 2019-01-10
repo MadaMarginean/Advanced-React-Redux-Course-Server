8a003cf7127ee958e9345c9b02f5a987941ac429
Madalina Marginean <madalina.marginean@mcro.tech>
2019-01-10 15:25:50 +0200

    Add cors to allow any port to access the server

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan'); // morgan is a login framework
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB Setup
 
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://localhost:auth/auth', { useNewUrlParser: true });
 // App Setup
 
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
